b8893945b3af86a06bd4b20f5b3b835293264abe
emrbs <mariaemerlitaorbiso21@gmail.com>
2022-02-12 10:13:04 +0800

    adding uiredux

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-//import { Provider } from 'react-redux';
-//import store from './redux/store';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
-  //<Provider store={store}>
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  //</Provider>,
+  </Provider>,
   document.getElementById('root')
 );
